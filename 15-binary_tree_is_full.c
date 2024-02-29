59a38d62b0c9c8f2900365435a05120404a516fa
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-02-29 10:02:31 -0800

    check

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL&& tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if (binary_tree_is_full(tree->left) &&
