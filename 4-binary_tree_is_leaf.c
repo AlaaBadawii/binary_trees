62611e187540c5d0e88e687c53c1d3f14e77d686
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-02-29 03:01:07 -0800

    check_4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,8 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->parent != NULL && node->left == NULL &&
-			node->right == NULL)
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	else
 		return (0);
