d981bcaec95fdcf41fd97c6fefce20f06f98ea76
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-02-29 01:48:14 -0800

    fix_betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * binary_tree_insert_left - function that inserts a node
+ * as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
- * @ value: the value to store in the new node
+ * @value: the value to store in the new node
  *
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
  */
